6bb1d740332d205e91fc75fabc0aafd9584de2e9
Firedrago95 <yonghwa95@naver.com>
2025-12-23 19:42:09 +0900

    refactor: ChatAnalyzeService 불필요한 코드 제거 및 구조 개선
    - 사용되지 않는 ChatBuffer 및 관련 의존성 제거
    - 주석 처리된 비동기 분석 메서드 코드 삭제
    - run 메서드에서 chatEmotionAnalyzer 분석 호출만 유지
    - 코드 가독성과 유지보수성을 향상

--- src/main/java/io/github/hypecycle/chatpipeline/analyzer/ChatAnalyzeService.java
+++ src/main/java/io/github/hypecycle/chatpipeline/analyzer/ChatAnalyzeService.java
@@ -1,14 +1,9 @@
 package io.github.hypecycle.chatpipeline.analyzer;
 
-import io.github.hypecycle.chatpipeline.buffer.ChatBuffer;
-import io.github.hypecycle.chatpipeline.domain.ChatMessage;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,24 +12,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChatAnalyzeService implements CommandLineRunner {
 
-    private final ChatBuffer chatBuffer;
     private final ChatEmotionAnalyzer chatEmotionAnalyzer;
 
     @Override
     public void run(String... args) throws Exception {
         chatEmotionAnalyzer.analyze();
     }
-
-//    @Async("chatManagerThreadPoolTaskExecutor")
-//    public void analyze() throws InterruptedException {
-//        while (!Thread.interrupted()) {
-//            List<ChatMessage> chatMessages = chatBuffer.drainBatch(30, 1000);
-//
-//            String collects = chatMessages.stream()
-//                    .map(ChatMessage::message)
-//                    .collect(Collectors.joining("/"));
-//            log.info("[ .. ] 배치 처리: {}", collects);
-//            chatEmotionAnalyzer.analyze(chatMessages);
-//        }
-//    }
 }
